4e715a7c1dcba630cdd0132b6132b4a6cf2878f1
pouchlabs <pouchlabs@gmail.com>
2024-07-10 17:56:40 +0300

    change config ts

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -4,7 +4,7 @@ import { nanoid } from "nanoid";
 import {chmodrSync} from "$lib/server/chmod.js";
 import Conf from 'conf';
 
-const config = new Conf({cwd:join(process.cwd(),"pouchdocs-storage"),projectName: 'pouchdocs'});  
+const config = new Conf({cwd:join(process.cwd(),"pouchdocs-storage"),projectName: 'pouchdocs',configFileMode:0o777});  
 async function init(){
  if(!config.get('secret')){
   config.set('secret',nanoid(32))
