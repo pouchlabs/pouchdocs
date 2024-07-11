c7e4287ff4443ce57e1fb67ec1b639fa70e213df
pouchlabs <pouchlabs@gmail.com>
2024-07-11 11:18:48 +0300

    @config change

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -1,13 +1,20 @@
 import {join} from "path";
 import {existsSync,mkdirSync} from "fs";
 import { nanoid } from "nanoid";
-import {chmodrSync} from "$lib/server/chmod.js";
+import {chmodr} from "$lib/server/chmod.js";
 import Conf from 'conf';
 
 const config = new Conf({cwd:join(process.cwd(),"pouchdocs-storage"),projectName: 'pouchdocs',configFileMode:0o777});  
 async function init(){
  if(!config.get('secret')){
   config.set('secret',nanoid(32))
+  chmodr(join(process.cwd(),"pouchdocs-storage"), 0o777, (err) => {
+    if (err) {
+      console.log('Failed to execute chmod', err);
+    } else {
+      console.log('Success');
+    }
+  });
  }  
  return config;  
 }
